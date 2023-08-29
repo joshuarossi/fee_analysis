94ce76f8a8ede9de52af75a60b9749817eaf2c2e
joshuarossi <maximojoshuarossi@gmail.com>
2023-08-29 16:15:09 -0600

    initial commit

---
+++ precache-manifest.52fc976696a77c48f33c8753e0ad8ed0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af95e4260e43511de8ae797c89602a3d",
+    "url": "/fee_calculator/index.html"
+  },
+  {
+    "revision": "8885bf5cbff0818543f1",
+    "url": "/fee_calculator/static/css/main.20750a48.chunk.css"
+  },
+  {
+    "revision": "d0566019cdc1922064cd",
+    "url": "/fee_calculator/static/js/2.8a346ba3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fee_calculator/static/js/2.8a346ba3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8885bf5cbff0818543f1",
+    "url": "/fee_calculator/static/js/main.04afd84b.chunk.js"
+  },
+  {
+    "revision": "21113d487adf35b35976",
+    "url": "/fee_calculator/static/js/runtime-main.d77d9f8b.js"
+  }
+]);
